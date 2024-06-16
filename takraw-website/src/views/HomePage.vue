7dc4869b3eee062d8d0d01cea206843c22634e5e
Salem Salleh <salem.salleh@getmercury.io>
2024-06-16 20:14:18 +0200

    Add root CSS template.

--- takraw-website/src/views/HomePage.vue
+++ takraw-website/src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="my-component">
-  </div>
+  <header class="site-header">
+  </header>
 </template>
 
 <script>
@@ -14,6 +14,37 @@ export default {
 </script>
 
 <style scoped>
-.my-component {
+:root {
+  --primary:;
+  --primary-pale:;
+  --primary-lightest:;
+  --primary-light:;
+  --primary-dark:;
+  --primary-darkest:;
+  --grey-00:;
+  --grey-10:;
+  --grey-20:;
+  --grey-30:;
+  --grey-40:;
+  --grey-50:;
+  --grey-60:;
+  --grey-70:;
+  --grey-80:;
+  --grey-90:;
+  --grey-100:;
+  --elevation-1:;
+  --elevation-2:;
+  --elevation-3:;
+  --accent-1:;
+  --accent-2:;
+  --accent-3:;
+  --highlight-1:;
+  --highlight-2:;
+  --highlight-3:;
+
+  --border-radius:;
+  --lines:;
+  --side-margin:;
+  --content-width:;
 }
 </style>
