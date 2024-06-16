7ed37ee1b318f711c545622590cad33799ee0f56
Salem Salleh <salem.salleh@getmercury.io>
2024-06-16 19:44:52 +0200

    Clean single component setup to start with.

---
+++ takraw-website/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-component">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
+
+<style scoped>
+.my-component {
+}
+</style>
