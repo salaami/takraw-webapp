b8498606e8a5221f880940c84bbc121dcf7fa7cf
Salaami <salemsalleh@googlemail.com>
2024-06-17 13:36:38 +0200

    CSS grid with holy grail layout.

--- takraw-website/src/views/HomePage.vue
+++ takraw-website/src/views/HomePage.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="site-header">
-  </header>
+  <div class="grid">
+    <header>Header</header>
+    <nav>Navigation</nav>
+    <article>Article</article>
+    <aside>Right Sidebar</aside>
+    <footer>Footer</footer>
+  </div>
 </template>
 
 <script>
@@ -40,5 +45,90 @@ export default {
   --highlight-1:;
   --highlight-2:;
   --highlight-3:;
+}
+@media(min-width: 320px){
+  :root {
+  --fs-huge: 3rem;
+  --fs-big: 1.75rem;
+  --fs-medium: 1.25rem;
+  --fs-normal: 1rem;
+  --fs-small: 0.75rem;
+  --fs-tiny: 0.5rem;
+  }
+}
+
+@media(min-width: 480px){
+  :root {
+  --fs-huge: 3.5rem;
+  --fs-big: 1.75rem;
+  --fs-medium: 1.25rem;
+  --fs-normal: 1rem;
+  --fs-small: 0.75rem;
+  --fs-tiny: 0.5rem;
+  }
+}
+
+@media(min-width: 768px){
+  :root {
+  --fs-huge: 6rem;
+  --fs-big: 3rem;
+  --fs-medium: 2rem;
+  --fs-normal: 1.25rem;
+  --fs-small: 1rem;
+  --fs-tiny: 0.75rem;
+  }
+}
+
+@media(min-width: 1024px){
+  :root {
+  --fs-huge: 6rem;
+  --fs-big: 3rem;
+  --fs-medium: 2rem;
+  --fs-normal: 1.5rem;
+  --fs-small: 1rem;
+  --fs-tiny: 0.75rem;
+  }
+}
+
+@media(min-width: 1200px){
+  :root {
+  --fs-huge: 6rem;
+  --fs-big: 3rem;
+  --fs-medium: 2rem;
+  --fs-normal: 1.5rem;
+  --fs-small: 1rem;
+  --fs-tiny: 0.75rem;
+  }
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat (3, 1fr);
+  grid-template-rows: 30% 200px auto;
+  grid-gap: 1em;
+}
+
+.grid > * {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+header,
+nav,
+article,
+aside,
+footer {
+  background: lightgray;
+  padding: 1em;
+}
+
+header,
+footer {
+  grid-column: 1 / 4;
+}
+
+article {
+  min-width: 15em;
 }
 </style>
